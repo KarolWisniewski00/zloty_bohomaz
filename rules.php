2e682e25b6acac552014e5e0fbe856ca1f49aea6
Karol Wiśniewski <karol.wisniewski2901@gmail.com>
2022-09-11 23:00:57 +0100

    Strona internetowa zloty bohomaz

---
+++ rules.php
@@ -0,0 +1,120 @@
+<!doctype html>
+<html lang = "pl">
+    <head>
+    <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Złoty bohomaz</title>
+        <link rel="icon" href="assets/main-icon.png" type="image/png">
+        <meta name="description" content="Złoty bohomaz Tattoo Studio - Najlepsze w Bytomiu i okolicy studio tatuażu, w którym artyści oddają całe serce do wykonywanej pracy">
+        <meta name="keywords" content="tattoo, tattoo bytom, bytom tattoo, bytom tatuaż, tatuaż bytom, tatuaż, studio tatuażu bytom, bytom studio tatuażu, studio tatuażu,
+        biszu tatuaż, bisz tattoo, biszusedziara, bisz tatuaż, złoty bohomaz, złoty bohomaz tattoo, złoty bohomaz tatuaż, złoty bohomaz bytom, bytom złoty bohomaz, biszu,
+        tatuaże, świat tatuażu, tatuaż dotpoint, czarny tatuaż, tatuaż blackwork, tattoo dotwork, tattoo blackwork, tatuaż dotwork śląsk, tatuaż blackwork śląsk
+        bezpieczeństwo, higiena, świat tatuażu, tatuaż śląsk, tatuaż katowice, tattoo katowice, tattoo śląsk, tattoo chorzów, tattoo gliwice, tatuaż chorzów, tatuaż gliwice, tatto piekary śląskie, tatuaż piekary śląskie">
+        <meta name="author" content="Karol Wiśniewski">
+        <meta http-equiv="X-Ua-Compatible" content="IE=edge">
+        
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+        <!--BOOTSTRAP 5 USING ONLY FOR MOVING SCREEN FRO LINK IN NAVSBAR-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+        <link rel="stylesheet" href="style/main.css">
+    </head>
+    <body>
+        <header>
+            <!--NAVBAR-->
+            <nav class="navbar navbar-dark bg-black navbar-expand-lg">
+                <div class="container justify-content-between">
+                    <a class="navbar-brand logo size-200 ml-1" href="#">Złoty Bohomaz</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainmenu" aria-controls="mainmenu" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-end" id="mainmenu">
+                        <ul class="navbar-nav text-center">
+                            <li class="nav-item"><a class="nav-link" href="index.php">Start</a></li>
+                            <li class="nav-item"><a class="nav-link" href="index.php#works">Prace</a></li>
+                            <li class="nav-item"><a class="nav-link" href="index.php#studio">Studio</a></li>
+                            <li class="nav-item"><a class="nav-link" href="index.php#artist">Artyści</a></li>
+                            <li class="nav-item"><a class="nav-link" href="index.php#price">Wycena</a></li>
+                            <li class="nav-item"><a class="nav-link" href="index.php#voucher">Voucher</a></li>
+                            <li class="nav-item"><a class="nav-link" href="index.php#heal">Gojenie</a></li>
+                            <li class="nav-item"><a class="nav-link" href="rules.php">Regulamin</a></li>
+                            <li class="nav-item"><a class="nav-link" href="index.php#contact">Kontakt</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <!--INSTA NAVBAR RIGHT-->
+            <div class="fixed-r">
+                <a href="https://www.instagram.com/zloty.bohomaz/"><img src="assets/ig-c.png" alt="instagram-icon"></a>
+            </div>
+        </header>
+
+        <main>
+            <!--RULES-->
+            <section id="rules">
+                <div class="container">
+                    <header>
+                        <span class="bg-icon"><img src="assets/documents.png" alt="rules-icon"></span>
+                        <h1 class="my-5">REGULAMIN</h1>
+                    </header>
+                    <hr class="my-4">
+                    <div class="row no-gutters justify-content-center">
+                        <div class="col-12 p-3">
+                            <p>Klient decydując się na wykonanie tatuażu w studiu tatuażu Złoty Bohomaz zobowiązany do zapoznania się z treścią regulaminu studia, przec co akceptuje jego warunki.</p>
+                        </div>
+                        <div class="col-12 p-3">
+                            <p>Studio wykonuje wszystkie zabiegi tylko osobom pełnoletnim. Osoba niepełnoletnia może zostać poddana zabiegowi wyłącznie za zgodą opiekuna prawnego</p>
+                        </div>
+                        <div class="col-12 p-3">
+                            <p>Klient ma obowiązek poinformować studio przed ustaleniem terminu sesji o wszelkich dolegliwościach/schorzeniach zdrowotnych, które w rezultacie mogą skutkować uniemożliwieniem wykonaia usługi tatuażu</p>
+                        </div>
+                        <div class="col-12 p-3">
+                            <p>Posiadam świadomość, że powikłania wynikające z chorób skórnych przy usłudze tatuażu, studio nie ponosi odpowiedzialności z tego wynikającej</p>
+                        </div>
+                        <div class="col-12 p-3">
+                            <p>Przed przystąpieniem do zabiegu każdy klient zobowiązany jest do wypełnienia oświadczenia: "Formularza dla klienta potwierdzającego zgodę na wykonanie tatuażu". Oświadczenie znajduje się w studio i klient wypełnia je przed sesją</p>
+                        </div>
+                        <div class="col-12 p-3">
+                            <p>Studio nie wykonuje zabiegów osobom będącym pod wpływem alkoholu, narkotyków i innych środków odurzających</p>
+                        </div>
+                        <div class="col-12 p-3">
+                            <p>Studio zapewnia sterylność używanych do zabiegu narzędzi i materiałów</p>
+                        </div>
+                        <div class="col-12 p-3">
+                            <p>W studio obowiązuje bezwzględny zakaz dotykania przedmiotów służacych do wykonania tatuażu</p>
+                        </div>
+                        <div class="col-12 p-3">
+                            <p>Zastrzegamy sobie prawa autorskie do wykonywanych zdjęć, projektów i tatuaży jak i do zdjęć naszych prac przesłanych przez klientów</p>
+                        </div>
+                        <div class="col-12 p-3">
+                            <p>W pomieszczeniu zabiegowym, podczas wykonywania zabiegu nie mogą przebywać osoby towarzyszące</p>
+                        </div>
+                        <div class="col-12 p-3">
+                            <p>Od 2020 roku zgodnie z wytycznymi sanepidu dla salonów tatuażu, osoby towarzyszące nie mogą także przebywać w poczekalni. Na umówiony tatuaż należy przychodzić samodzielnie</p>
+                        </div>
+                        <div class="col-12 p-3">
+                            <p>W studiu obowiązuje całkowity zakaz palenia tytoniu oraz spożywania alkoholu lub innych środków odurzających</p>
+                        </div>
+                        <div class="col-12 p-3">
+                            <p>Zabrania się przyprowadzania zwierząt</p>
+                        </div>
+                        <div class="col-12 p-3">
+                            <p>W przypadku gdy tatuaż wymaga zdjęcia ubrania klient ma prawo prosić o wydzielenie stanowiska za pomocą parawanu. W związku z koniecznością odpowiedniego napięcia skóry, w miejscu tatuażu, artysta ma prawo zażądać odsłonięcia większej części ciała lub zdjęcia danej części ubrania/bielizny aby poprawnie wykonać zabieg</p>
+                        </div>
+                        <div class="col-12 p-3">
+                            <p>Materiały zużyte prz tatuażu, takie jak: igły, uchwyty do maszyn, kubeczki na tusz wraz z tuszem są utylizowane za pośrednictwem wyspecjalizowanej firmy zajmującej się odbiorem materiałów skażonych drobnoustrojami</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!--FOOTER-->
+            <footer class="footer mt-5 py-3 bg-black text-white">
+                <div class="container">
+                    <div class="row ml-1">© 2022 Created by Karol Wiśniewski</div>
+                    <div class="row ml-1">All rights reserved</div>
+                </div>
+            </footer>
+        </main>
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+    </body>
+</html>
